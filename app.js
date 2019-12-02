fb484e6dea7ffcf4d55e48c9048d191a3e535436
Yintong <zhouyintong96@gmail.com>
2019-12-02 15:27:43 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/auth_demo_app');
+
 var app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('', function(req, res){
+    res.render("home");
+});
+
+app.get('/secret', function(req, res){
+    res.render('secret');
+});
+
+//collemaneto server port
+app.listen(3000, () => console.log('Express Server is running!'));
