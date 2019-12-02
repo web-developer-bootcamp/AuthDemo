92f39d92c060274245c73a5f197d24a8a6ed5532
Yintong <zhouyintong96@gmail.com>
2019-12-02 15:44:05 +0100

    update passport

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-var express = require('express');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/auth_demo_app');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    localStrategy = require('passport-local'),
+    passportLocalMongoose = require('passport-local-mongoose'),
+    User = require('./models/user');
 
+//db connection
+mongoose.connect('mongodb://localhost/auth_demo_app', {useNewUrlParser: true});
 var app = express();
+
+//passport session
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set('view engine', 'ejs');
 
-app.get('', function(req, res){
+app.use(require('express-session')({
+    secret: 'Rusty is the best and cutest dog in the world',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//home rendering 
+app.get('', function (req, res) {
     res.render("home");
 });
 
-app.get('/secret', function(req, res){
+//secret page rendering
+app.get('/secret', function (req, res) {
     res.render('secret');
 });
 
